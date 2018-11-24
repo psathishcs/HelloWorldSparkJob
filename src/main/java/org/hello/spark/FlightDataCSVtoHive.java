e3e83021f1a1b1d0029b5f67b081774f61094000
Sathish Kumar Parthasarathy <p.sathish.cs@gmail.com>
2018-11-23 20:33:28 -0500

    fix

--- src/main/java/org/hello/spark/FlightDataCSVtoHive.java
+++ src/main/java/org/hello/spark/FlightDataCSVtoHive.java
@@ -14,6 +14,7 @@ public class FlightDataCSVtoHive {
 			System.out.println("No files provided");
 			System.exit(0);
 		}
+		String fileName = args[0];
 		SparkConf conf = new SparkConf();
 		if (System.getenv("env") != null && System.getenv("env").equals("local")) {
 			conf.setMaster("local")
@@ -25,7 +26,7 @@ public class FlightDataCSVtoHive {
 				.appName("Read flight CSV data and write to flight_data Hive table")
 				.enableHiveSupport()
 				.getOrCreate();
-		Dataset<Row> flightData = readCSV(args[0]);
+		Dataset<Row> flightData = readCSV(fileName);
 		flightData.show();
 		flightData.write().mode(SaveMode.Overwrite).saveAsTable("flight.flight_data");
 	}
--- src/main/java/org/hello/spark/FlightDataKafkaToHive.java
+++ src/main/java/org/hello/spark/FlightDataKafkaToHive.java
@@ -9,26 +9,27 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
 import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.kafka010.ConsumerStrategies;
 import org.apache.spark.streaming.kafka010.KafkaUtils;
 import org.apache.spark.streaming.kafka010.LocationStrategies;
 
+
 import scala.Tuple2;
 
-/**
- * 
- * @author SathishParthasarathy
- *
- */
-public class HelloWorldKafkaStreaming {
+public class FlightDataKafkaToHive {
+	static SparkSession spark;
 	public static void main(String[] args) throws InterruptedException {
 		SparkConf conf = new SparkConf().setMaster("local").setAppName("Kafka Spark Streaming");
+		conf.set("hive.metastore.uris", "thrift://skylark.datalake:9083");
 		JavaSparkContext sc = new JavaSparkContext(conf);
 		JavaStreamingContext ssc = new JavaStreamingContext(sc, Durations.seconds(1));
 		sc.setLogLevel("WARN");
+		
 
 		Map<String, Object> kafkaParams = new HashMap<String, Object>();
 		kafkaParams.put("bootstrap.servers", "skylark.datalake:9092");
@@ -37,17 +38,16 @@ public class HelloWorldKafkaStreaming {
 		kafkaParams.put("group.id", "1");
 		kafkaParams.put("auto.offset.reset", "latest");
 		kafkaParams.put("enable.auto.commit", false);
-
-		Collection<String> topics = Arrays.asList("hello_kafka");
+		
+		Collection<String> topics = Arrays.asList("flight_data");
 		JavaInputDStream<ConsumerRecord<String, String>> stream = 
 				KafkaUtils.createDirectStream(ssc,
 				LocationStrategies.PreferConsistent(),
 				ConsumerStrategies.<String, String>Subscribe(topics, kafkaParams));
-		stream.mapToPair(record -> new Tuple2<>(record.key(), record.value())).print();
-		stream.map(record -> record.value()).print();
-	
+		
 		ssc.start();
 		ssc.awaitTermination();
 
+		
 	}
 }
